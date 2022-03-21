9ae90e4885bdc6f2d3c04cb4935b5bbc67cb48bc
Miguel-Lopez-97 <miguel.m.lopez.p@gmail.com>
2022-03-21 12:16:55 -0500

    Update CSS general styles

--- frontend/src/views/dashboard/superAdmin/superAdminDashboard.jsx
+++ frontend/src/views/dashboard/superAdmin/superAdminDashboard.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "./styles.module.css"
 
 export function SuperAdminDashboard() {
   return (
-    <div>
+    <div className={styles.example}>
       <h1>Tablero de superAdminDashboard</h1>
     </div>
   );
