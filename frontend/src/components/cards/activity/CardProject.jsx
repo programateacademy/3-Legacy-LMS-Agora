3908006d195b37767ff34b016362bf248b5054ea
MatthewHDemon <danielmateopatarroyomolina@gmail.com>
2022-03-23 18:38:10 -0500

    update cardProyect

--- frontend/src/components/cards/activity/CardProject.jsx
+++ frontend/src/components/cards/activity/CardProject.jsx
@@ -7,7 +7,6 @@ export function CardProject() {
       <ul className={styles.cards}>
   <li>
     <a href="" className={styles.card}>
-      <img src="https://i.imgur.com/oYiTqum.jpg" className={styles.card__image} alt="" />
       <div className={styles.card__overlay}>
         <div className={styles.card__header}>
           <svg className={styles.card__arc} xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
@@ -17,20 +16,6 @@ export function CardProject() {
           </div>
         </div>
         <p className={styles.card__description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-		{/* <a href="#" className={styles.dots}>
-  		<div className={styles.dot}>
-        </div>
-		</a> */}
-    <details>
-  <summary></summary>
-  <nav class={styles.menu}>
-    <a href="#link">Home</a>
-    <a href="#link">Work</a>
-    <a href="#link">Links</a>
-    <a href="#link">Contact</a>
-    <a href="#link">About</a>
-  </nav>
-</details>
 	  </div>
     </a>
   </li> 
