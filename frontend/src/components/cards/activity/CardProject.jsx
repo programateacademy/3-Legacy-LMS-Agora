52a49db8ba8b103d4b88816baf43f71f796a46f7
MatthewHDemon <danielmateopatarroyomolina@gmail.com>
2022-03-24 19:02:47 -0500

    Update cardProyect

--- frontend/src/components/cards/activity/CardProject.jsx
+++ frontend/src/components/cards/activity/CardProject.jsx
@@ -1,25 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from './styles.module.css';
 
 export function CardProject() {
   return (
-    <div className={styles.projectCard}>
-      <ul className={styles.cards}>
-  <li>
-    <a href="" className={styles.card}>
-      <div className={styles.card__overlay}>
-        <div className={styles.card__header}>
-          <svg className={styles.card__arc} xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-          <div className={styles.card__header_text}>
-			  <h2>Project</h2>
-            <span className={styles.card__status}>1 hour ago</span>
+    <ul className={styles.cards}>
+      <li>
+        <div className={styles.card}>
+          <div className={styles.dots}>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+            <div className={styles.list}>
+              <li>
+                Editar
+              </li>
+              <li>
+                Calificar
+              </li>
+            </div>
+            <div className={styles.dot}></div>
+          </div>
+          <img src="https://i.imgur.com/oYiTqum.jpg" className={styles.card__image} alt="" />
+          <div className={styles.card__overlay}>
+            <div className={styles.card__header}>
+              <path />
+              <div className={styles.card__header__text}>
+                <h1 className={styles.card__title}>PROJECT</h1>
+              </div>
+            </div>
+            <p className={styles.card__description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
           </div>
         </div>
-        <p className={styles.card__description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-	  </div>
-    </a>
-  </li> 
-</ul>
-    </div>
+      </li>
+    </ul>
   );
 }
