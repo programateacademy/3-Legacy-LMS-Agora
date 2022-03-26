f79cdd7c177d85fa337c9ca8df5aa517730eb51e
Miguel-Lopez-97 <miguel.m.lopez.p@gmail.com>
2022-03-26 17:38:03 -0500

    update backend middleware route cohort

--- backend/src/routes/competence.js
+++ backend/src/routes/competence.js
@@ -7,6 +7,6 @@ router.post('/new-competence', authAdmin, controllerCompetence.create);
 router.get('/get-competences/:_id', authUser, controllerCompetence.getCompetences);
 router.get('/get-competence/:_id', authAdmin, controllerCompetence.getCompetence);
 router.put('/update-competence/:_id', authAdmin, controllerCompetence.updateCompetence)
-router.delete('/delete-competence/:_id', authUser, controllerCompetence.deleteCompetence);
+router.delete('/delete-competence/:_id', authAdmin, controllerCompetence.deleteCompetence);
 
 module.exports = router
