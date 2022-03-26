933bb8f969cbefdee1e8c5cb9a04621b456bb344
ValentinaRippe <lauravalentina200102@gmail.com>
2022-03-26 17:27:48 -0500

    add dising

--- frontend/src/views/dashboard/admin/AdminDashboard.jsx
+++ frontend/src/views/dashboard/admin/AdminDashboard.jsx
@@ -5,7 +5,9 @@ import { Button } from "../../../components/buttons/Button/Button.jsx";
 export function AdminDashboard() {
   return (
     <div className={styles.adminDashboard}>
-      <h2>Bootcamps</h2>
+      <div class={styles.wrapper}>
+        <h2 class={styles.typing_demo}>Bootcamps</h2>
+      </div>
       <div className={styles.buttonsContainer}>
         <Button title="Crear bootcamp" link="/bootcamp/create-bootcamp" />
         <Button title="Ver formadores" link="/list_teachers" />
