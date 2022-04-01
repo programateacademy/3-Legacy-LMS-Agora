059e02ea4b84c39280700d4713f5374fae262c68
mint <estefanyazocar3@gmail.com>
2022-04-01 14:50:08 -0500

    profile get info

--- backend/src/routes/profile.js
+++ backend/src/routes/profile.js
@@ -1,12 +1,12 @@
 const router = require('express').Router()
-const auth = require('../middleware/auth')
+const authUser = require('../middleware/authUser')
 const authTeacher = require('../middleware/authTeacher')
 const authAdmin = require('../middleware/authAdmin')
 const controllerProfile = require('../controllers/profile')
 
 router.post('/new-profile', controllerProfile.create);
 router.get('/get-profiles/:_id', controllerProfile.getProfiles);
-router.get('/get-profile/:_id', controllerProfile.getProfile);
+router.get('/get-profile/:_id', authUser , controllerProfile.getProfile);
 router.put('/update-profile/:_id', controllerProfile.updateProfile)
 router.put('/update-profile-competence/:_id', controllerProfile.updateCompetence)
 router.delete('/delete-profile/:_id', controllerProfile.deleteProfile);
--- frontend/src/components/ProfileStudent/ProfileStudent.jsx
+++ frontend/src/components/ProfileStudent/ProfileStudent.jsx
@@ -4,6 +4,7 @@ import { BsArrowLeftCircle } from "react-icons/bs";
 import {AiFillLinkedin} from "react-icons/ai"
 import {AiFillGithub} from "react-icons/ai"
 import {FaNewspaper} from "react-icons/fa"
+import {RiCake2Fill} from "react-icons/ri"
 import {useNavigate} from "react-router-dom"
 import {TableStudent} from "./TableStudent"
 import { CompetencesTable } from "../competencesTable/CompetencesTable";
@@ -11,12 +12,32 @@ import { useEffect, useState } from "react";
 import {useSelector} from "react-redux";
 import apiAgora from "../../api"
 
+const initialState = {
+    competence:[],
+    dateOfBirth:"",
+    gitHub:"",
+    image:"",
+    portafolio:"",
+    linkedin:""
+}
+
 export function ProfileStudent(){
         const auth = useSelector((state) => state.auth);
         const userID = auth.user.id;
         const cohortID = auth.user.cohortID;
         let navigate = useNavigate();
         const [cohortCompetences, setCohortCompetences] = useState([]);
+        const [user, setUser] = useState([]);
+        const [userProfile, setUserProfile] = useState(initialState);
+
+        const {
+            competence,
+            dateOfBirth,
+            gitHub,
+            image,
+            portafolio,
+            linkedin
+        } = userProfile;
       
         const fetchCohortCompetences = async (url, id) => {
           const resCompetencesCohort = await apiAgora.get(
@@ -28,13 +49,41 @@ export function ProfileStudent(){
           const res = resCompetencesCohort.data
           setCohortCompetences(res);
         };
+
+        const fetchUser = async (id) =>{
+            const resUser = await apiAgora.get(
+                `/api/get_user/${id}`,
+            {
+              headers: { Authorization: id },
+            }
+          );
+          const res = resUser.data
+          setUser(res);
+        };
+        
+        const fetchUserProfile = async (id) =>{
+            const resUserProfile = await apiAgora.get(
+                `/api/agora/get-profile/${id}`,
+            {
+              headers: { Authorization: id },
+            }
+          );
           
+          const res = resUserProfile.data
+          res.dateOfBirth = new Date(res.dateOfBirth).toLocaleDateString("en-CA")
+          setUserProfile(res);
+        };
+        const handleChangeInput = (e) => { const { name, value } = e.target; setUserProfile({ ...userProfile, [name]: value, err: "", success: "" }); }; 
+        
         useEffect(() => {
           fetchCohortCompetences(cohortID, userID);
+          fetchUser(userID);
+          fetchUserProfile(userID);
+
         }, [cohortID, userID]);
     return(
         <div className={styles.container}>
-            <div className={styles.containerProfile}>
+            <form className={styles.containerProfile}>
                     <button className={styles.button_return} onClick={() => navigate(-1)}>
                         <BsArrowLeftCircle size={30} />
                     </button>
@@ -43,33 +92,38 @@ export function ProfileStudent(){
                     <div className={styles.cajaUlt}>
                         <input
                             className={styles.input__imageURL}
-                            placeholder="Inserta URL de la imagen de la Consulta"
+                            placeholder="Inserta URL de la imagen"
                             type="text"
-                            name="pictureQuery"
+                            name="image" value={image}
+                            onChange={handleChangeInput}
                         />
                     </div>
                 </div>
                 <div className={styles.cajaName}>
-                    <h2>Nombre del estudiante</h2>
+                    <h2>{user.firstName+ " " + user.middleName+ " " + user.lastName + " " + user.secondSurname}</h2>
                 </div>
                 <div className={styles.cajaLink}>
                     <div className={styles.cajaUlt}>
-                        <input type="text" placeholder="Enlace Linkedin" />
+                        <input type="text" placeholder="Enlace Linkedin" name="linkedin" value={linkedin} onChange={handleChangeInput} />
                         <AiFillLinkedin size={30} color="#FEFEFE"/>
                     </div>
                     <div className={styles.cajaUlt}>
-                        <input type="text" placeholder="Enlace Github"/>
+                        <input type="text" placeholder="Enlace Github" name="gitHub" value={gitHub} onChange={handleChangeInput}/>
                         <AiFillGithub size={30} color="#FEFEFE"/>
                     </div>
                     <div className={styles.cajaUlt}>
-                        <input type="text" placeholder="Enlace Portafolio"/>
+                        <input type="text" placeholder="Enlace Portafolio" name="portafolio" value={portafolio} onChange={handleChangeInput}/>
                         <FaNewspaper size={30} color="#FEFEFE"/>
+                    </div>
+                    <div className={styles.cajaUlt}>
+                        <input type="date" name="dateOfBirth" value={dateOfBirth} onChange={handleChangeInput}/>
+                        <RiCake2Fill size={30} color="#FEFEFE"/>
                     </div>
                     <div className={styles.cajaUlt}>
                         <button>Confirmar</button>
                     </div>
                 </div>
-            </div>
+            </form>
             <div className={styles.tableCompetences}>
                 
                     <TableStudent/>
