70aab55b744462c6e57a8bccb39b3f8d7b2f7594
Jhonattan Steven Saavedra Gomez <80645321+JhonattanSSG01@users.noreply.github.com>
2023-02-08 16:38:42 -0500

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -5,5 +5,5 @@ import axios from "axios"
   }); */
 
 export default axios.create({
-  baseURL: "https://3-legacy-lms-agora-frontend-dqq5.vercel.app",
+  baseURL: "https://3-legacy-lms-agora-frontend.vercel.app",
   });
