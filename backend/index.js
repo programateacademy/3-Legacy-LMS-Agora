89a413426d8a77a492bb15b66faf3ceafe47eeef
Jhonattan Saavedra Gomez <jhonattanestivensaavedragomez@gmail.com>
2023-02-08 15:14:19 -0500

    Agregando url de back

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
-// import axios from "axios"
+import axios from "axios"
 
-// export default axios.create({
-//     baseURL: "https://agora-programate-lms.herokuapp.com/",
-//   });
+export default axios.create({
+  baseURL: "https://3-legacy-lms-agora-frontend.vercel.app",
+  });
