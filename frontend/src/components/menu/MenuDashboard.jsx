9e289b49a009f79823024ccbf0055edc922a7e9f
Kevin Arias <kariasduarte@gmail.com>
2023-02-15 12:39:51 -0500

    4

--- frontend/src/components/menu/MenuDashboard.jsx
+++ frontend/src/components/menu/MenuDashboard.jsx
@@ -4,37 +4,30 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import style from "./MenuDashboard.module.css";
 import "../header/UserLink.css";
-<<<<<<< HEAD
-=======
 import { BsFillFileCodeFill } from "react-icons/bs";
 import { AiOutlineFundProjectionScreen, AiFillProject } from "react-icons/ai";
 import { MdAnnouncement } from "react-icons/md";
 import { FiFileText } from "react-icons/fi";
 import { RiFileUserFill, RiPagesFill } from "react-icons/ri";
->>>>>>> b5a9603 (Probando creciones de objetos y limpiando el proyecto un poco)
 import { IconContext } from "react-icons";
 import { useParams } from "react-router-dom";
 
 export function MenuDashboard({ open, setOpen }) {
   const auth = useSelector((state) => state.auth);
   const params = useParams();
-  const cohortID = params.id;
+  const cohortID = params._id;
   const { isTeacher } = auth;
   const [activeLink, setActiveLink] = useState(null);
   const navLinks = [
-    // {
-    //   text: "Estadísticas",
-    //   route: `/dashboard/${cohortID}/statistics`,
-    //   icon: <AiOutlineFundProjectionScreen className={style.icon} />,
-    // }, 
+    {
+      text: "Estadísticas",
+      route: `/dashboard/${cohortID}/statistics`,
+      icon: <AiOutlineFundProjectionScreen className={style.icon} />,
+    }, 
     {
       text: "Proyectos",
       route: `/dashboard/${cohortID}/projects`,
-<<<<<<< HEAD
       icon: <i className={`ri-bar-chart-box-line icon ${style.icon}`}></i>,
-=======
-      icon: <i className={`ri-bar-chart-box-line ${style.icon}`}></i>
->>>>>>> 349814f (merge arreglado)
     },
     {
       text: "Workbooks",
