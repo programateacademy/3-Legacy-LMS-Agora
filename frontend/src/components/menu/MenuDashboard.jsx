b9539ef028af154c70f08e75b5516288b9a382db
Jhonattan Saavedra Gomez <jhonattanestivensaavedragomez@gmail.com>
2023-02-20 18:27:51 -0500

    Solucionando el menu desplegable

--- frontend/src/components/menu/MenuDashboard.jsx
+++ frontend/src/components/menu/MenuDashboard.jsx
@@ -12,7 +12,6 @@ export function MenuDashboard({ open, setOpen }) {
   const cohortID = params.id;
   const { isTeacher } = auth;
   const [activeLink, setActiveLink] = useState(null);
-  console.log(cohortID)
   const navLinks = [
     {
       text: "Estadísticas",
