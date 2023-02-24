412ac5d7123862e6dcc3844d4eecdcc8596220a5
Jhonattan Saavedra Gomez <jhonattanestivensaavedragomez@gmail.com>
2023-02-24 14:33:11 -0500

    Solucionando error vercel

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,20 +1,20 @@
-import '@testing-library/jest-dom'
+// import '@testing-library/jest-dom'
 
-// Mock IntersectionObserver
-class IntersectionObserver {
-  observe = jest.fn()
-  disconnect = jest.fn()
-  unobserve = jest.fn()
-}
+// // Mock IntersectionObserver
+// class IntersectionObserver {
+//   observe = jest.fn()
+//   disconnect = jest.fn()
+//   unobserve = jest.fn()
+// }
 
-Object.defineProperty(window, 'IntersectionObserver', {
-  writable: true,
-  configurable: true,
-  value: IntersectionObserver,
-})
+// Object.defineProperty(window, 'IntersectionObserver', {
+//   writable: true,
+//   configurable: true,
+//   value: IntersectionObserver,
+// })
 
-Object.defineProperty(global, 'IntersectionObserver', {
-  writable: true,
-  configurable: true,
-  value: IntersectionObserver,
-})
+// Object.defineProperty(global, 'IntersectionObserver', {
+//   writable: true,
+//   configurable: true,
+//   value: IntersectionObserver,
+// })
